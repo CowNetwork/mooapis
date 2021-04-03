4ee996028a9cdce7960fe37220734ae185f1c07d
Tobias B <germansuperioz@yahoo.de>
2021-04-03 15:05:20 +0200

    Update install-buf.sh

--- install-buf.sh
+++ install-buf.sh
@@ -2,7 +2,5 @@
 BUF_VERSION=0.41.0
 BIN=/usr/local/bin
 
-mkdir -p "$BIN"
-
-curl -sSL "https://github.com/bufbuild/buf/releases/download/v$BUF_VERSION/buf-Linux-x86_64" -o "$BIN/buf"
-chmod +x "$BIN/buf"
+sudo curl -sSL "https://github.com/bufbuild/buf/releases/download/v$BUF_VERSION/buf-Linux-x86_64" -o "$BIN/buf"
+sudo chmod +x "$BIN/buf"
