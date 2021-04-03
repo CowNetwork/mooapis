a0490e0b56c8644d8bb76195a57f9e7c7e25ad6f
Tobias B <germansuperioz@yahoo.de>
2021-04-03 14:50:05 +0200

    Update install-buf.sh

--- install-buf.sh
+++ install-buf.sh
@@ -5,7 +5,7 @@ CACHE=$HOME/.cache/bin
 export PATH=$PATH:$CACHE
 export GOBIN=$CACHE
 
-mkdir -p "$CACHE_BIN"
+mkdir -p "$CACHE"
 
-curl -sSL "https://github.com/bufbuild/buf/releases/download/v$BUF_VERSION/buf-Linux-x86_64" -o "$CACHE_BIN/buf"
-chmod +x "$CACHE_BIN/buf"
+curl -sSL "https://github.com/bufbuild/buf/releases/download/v$BUF_VERSION/buf-Linux-x86_64" -o "$CACHE/buf"
+chmod +x "$CACHE/buf"
