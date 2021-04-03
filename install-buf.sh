94e33be52c5350088f9cbaa1444b19910e49e8b9
Tobias B <germansuperioz@yahoo.de>
2021-04-03 14:39:32 +0200

    Try #2

--- install-buf.sh
+++ install-buf.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 BUF_VERSION=0.41.0
 CACHE_BASE=$HOME/.cache
-CACHE_BIN=$CACHE/bin
+CACHE_BIN=$CACHE_BASE/bin
 
 export PATH=$PATH:$CACHE_BIN
 export GOBIN=$CACHE_BIN
+
+mkdir -p "$CACHE_BIN"
 
 curl -sSL "https://github.com/bufbuild/buf/releases/download/v$BUF_VERSION/buf-Linux-x86_64" -o "$CACHE_BIN/buf"
 chmod +x "$CACHE_BIN/buf"
