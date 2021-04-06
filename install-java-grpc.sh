9b05f9391043f38d4847293667cf04bb6b92e2ac
Tobias B <germansuperioz@yahoo.de>
2021-04-06 09:08:28 +0200

    Update ci.yaml

---
+++ install-java-grpc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+VERSION=1.24.0
+BIN=/usr/local/bin
+
+sudo curl -sSL "https://repo1.maven.org/maven2/io/grpc/protoc-gen-grpc-java/$VERSION/protoc-gen-grpc-java-$VERSION-linux-x86_64.exe" -o "$BIN/protoc-gen-grpc-java"
+sudo chmod +x "$BIN/protoc-gen-grpc-java"
